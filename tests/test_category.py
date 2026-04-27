1018ac482659abf8d53787aff474bf85220df394
Александр <shura.yablonskij@mail.ru>
2026-04-27 20:25:16 +0300

    feat: complete homework 14.2 - encapsulation, getters, setters, classmethod, tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,3 +1,4 @@
+
 from src.category import Category
 from src.product import Product
 
@@ -6,24 +7,34 @@ class TestCategory:
     def test_category_initialization(self):
         products = [
             Product("Телефон", "Смартфон", 30000, 5),
-            Product("Чехол", "Защитный чехол", 1000, 15),
+            Product("Чехол", "Защитный", 1000, 15),
         ]
         category = Category("Электроника", "Все гаджеты", products)
-
         assert category.name == "Электроника"
         assert category.description == "Все гаджеты"
-        assert len(category.products) == 2
 
     def test_category_count_increment(self):
         initial_count = Category.category_count
-        Category("Книги", "Художественная литература", [])
+        Category("Книги", "Художественная", [])
         assert Category.category_count == initial_count + 1
 
     def test_product_count_increment(self):
         initial_count = Category.product_count
-        products = [
-            Product("Книга", "Роман", 500, 10),
-            Product("Журнал", "Глянцевый", 300, 20),
-        ]
+        products = [Product("Книга", "Роман", 500, 10)]
         Category("Книги", "Литература", products)
-        assert Category.product_count == initial_count + 2
+        assert Category.product_count == initial_count + 1
+
+    def test_add_product(self):
+        category = Category("Игрушки", "Детские", [])
+        product = Product("Кукла", "Игрушка", 1500, 5)
+        initial_count = Category.product_count
+        category.add_product(product)
+        assert Category.product_count == initial_count + 1
+
+    def test_products_getter(self):
+        product1 = Product("Телефон", "Смартфон", 30000, 5)
+        product2 = Product("Чехол", "Защитный", 1000, 15)
+        category = Category("Электроника", "Гаджеты", [product1, product2])
+        result = category.products
+        assert "Телефон, 30000 руб. Остаток: 5 шт." in result
+        assert "Чехол, 1000 руб. Остаток: 15 шт." in result
